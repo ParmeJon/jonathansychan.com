a55629ab28ac487468dd335de0c07f470408718e
github username <jonathansychan@gmail.com>
2019-10-15 16:15:21 -0400

    Added new icons

--- src/components/icons.js
+++ src/components/icons.js
@@ -3,32 +3,50 @@ import React from 'react';
 function Icons() {
   return (
     <div className="external-links">
-    <a
-    className="linkedin-icon"
-    href="https://www.linkedin.com/in/jonathansychan/"
-    target="_blank"
-    rel="noopener noreferrer"
-    >
-    <i className="fab fa-linkedin"></i>
-    </a>
+      <a
+        className="linkedin-icon"
+        href="https://www.linkedin.com/in/jonathansychan/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className="fab fa-linkedin"></i>
+      </a>
 
-    <a
-    className="youtube-icon"
-    href="https://www.youtube.com/jonsyvids"
-    target="_blank"
-    rel="noopener noreferrer"
-    >
-    <i className="fab fa-youtube"></i>
-    </a>
-    <a
-    className="github-icon"
-    href="https://www.github.com/parmejon"
-    target="_blank"
-    rel="noopener noreferrer"
-    >
-    <i className="fab fa-github"></i>
-    </a>
+      {/* <a
+        className="youtube-icon"
+        href="https://www.youtube.com/jonsyvids"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className="fab fa-youtube"></i>
+      </a> */}
+      <a
+        className="github-icon"
+        href="https://www.github.com/parmejon"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className="fab fa-github"></i>
+      </a>
+
+      <a
+        className="ig-icon"
+        href="https://www.instagram.com/parmejondesigns/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className="fab fa-instagram"></i>
+      </a>
+
+      <a
+        className="iot-icon"
+        href="https://parmejon.github.io/Inanimate-Object-Test/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className="far fa-smile"></i>
+      </a>
     </div>
-  )
+  );
 }
 export default Icons
