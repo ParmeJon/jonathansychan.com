98732828b6faf894c3fa8444fee82ff4842a2a48
github username <jonathansychan@gmail.com>
2020-07-14 00:58:08 -0400

    update personal website

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
             <h1 className="main-title">Jonathan</h1>
             <h1 className="main-title"> SY </h1>
             <h1 className="main-title">Chan</h1>
-            <i className="main-description">an always learning software engineer and digital artist </i>
+            <i className="main-description">a tenacious software engineer and digital artist </i>
           </div>
 
           <div className="image-container">
